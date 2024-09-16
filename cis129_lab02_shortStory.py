a6bac9c37119e614a727ebd86148175e1dced850
mbrady33 <mbrady19@mail.pima.edu>
2024-09-16 16:13:35 -0700

    ssl_cis129_lab02_shortStory.py
    prints a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,9 @@
-print('Hello, World!')
+# Author: Michaela Brady
+# My first Python lab in my CIS129 course
+# Prints a simple short story
 
+print('He looked in my eyes and laughed.')
+print('So I smiled back in pain')
+print('I felt happy, knowing he would never laugh again')
+print('For as long as I shall breathe the life of family who cannot')
+print('he will pay for every laugh and every hug that I have lost')
